2f50391cc8075de7731f3249b306c352c0d69228
Alexander Pridava <sanyapridava@mail.ru>
2024-04-12 01:50:45 +0300

    Create hidden window for choice files programms

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, {lazy, Suspense, useState} from 'react';
 import Window from "./components/Window";
+import TranslatorControl from "./components/TranslatorControl";
+import SpeedControl from "./components/TranslatorControl/SpeedControl";
+import ButtonRun from "./components/TranslatorControl/ButtonRun";
+import ButtonChoice from "./components/TranslatorControl/ButtonChoice";
+import ButtonStop from "./components/TranslatorControl/ButtonStop";
 
+const WindowFiles = lazy(() : any =>{
+    import("./components/WindowFiles");
+});
+ 
 function App() {
+    
+    const [status_work, changeStatusWork] = useState(false);
+    
+    const setStateWork = (mode: boolean)=>{
+        changeStatusWork(mode);
+    }
+    
     return (
         <div>
             <h1 className={'text-center text-xl p-10'}>Translator C# code to TS</h1>
 
-            <div className={'grid grid-cols-2'}>
+            <div className={'grid grid-cols-2 ml-20'}>
                 <Window label={"Исходный код"} className={'bg-blue-950 w-3/4 h-80 rounded'}/>
                 <Window label={"Результат"} className={'bg-blue-950 w-3/4 h-80 rounded'}/>
             </div>
+
+            {
+                status_work && <Suspense>
+                    <WindowFiles/>
+                </Suspense>
+            }
+            
+            <TranslatorControl>
+                <ButtonRun onClick={setStateWork(true)}/>
+                <ButtonStop/>
+                <SpeedControl/>
+                <ButtonChoice/>
+            </TranslatorControl>
         </div>
 
 
---
+++ gui/src/components/WindowFiles.tsx
@@ -0,0 +1,10 @@
+
+export default function WindowFiles() {
+    
+    return <div id={"background"} className={"bg-black opacity-30 flex"} style={{}}>
+        <div id={"Choice"} className={"bg-white opacity-100 rounded w-80 h-40"}>
+            
+        </div>
+    </div>
+    
+}

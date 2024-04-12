3ebc347365e59fcb22ce6f5bb216b14e8bc7c7c5
Alexander Pridava <sanyapridava@mail.ru>
2024-04-12 12:58:14 +0300

    Create class with static method for request to server

--- gui/src/API/APImethod.ts
+++ gui/src/API/APImethod.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 export class API {
     
     static getTextProgram() {
-        
+        axios.get("http://localhost:5123/Home/Index").then(res=>console.log(res))
+            .catch((err)=>alert("Север не ответил("));
     }
     
 }

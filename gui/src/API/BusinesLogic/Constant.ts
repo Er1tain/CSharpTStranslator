fc7a20ef30c45857d772ba6fa67d4c246084171f
Alexander Pridava <sanyapridava@mail.ru>
2024-04-13 12:41:06 +0300

    Classes for work with leksem

---
+++ gui/src/API/BusinesLogic/Constant.ts
@@ -0,0 +1,45 @@
+import {Leksem, leksem} from "./Leksem";
+
+export class NumericConst implements Leksem {
+
+    public readonly value: string;
+    public readonly type: string;
+    public readonly color;
+
+    constructor(value: string, type: string) {
+        this.value = value;
+        this.type = type;
+        this.color = "purple";
+    }
+
+
+    public getFormatLeksem(): leksem {
+        return {
+            value: this.value,
+            color: this.color
+        }
+    }
+
+}
+
+export class StringConst implements Leksem {
+
+    public readonly value: string;
+    public readonly type: string;
+    public readonly color;
+
+    constructor(value: string, type: string) {
+        this.value = value;
+        this.type = type;
+        this.color = "green";
+    }
+
+
+    public getFormatLeksem(): leksem {
+        return {
+            value: this.value,
+            color: this.color
+        }
+    }
+
+}

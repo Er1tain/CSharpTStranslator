a89adb12dc2b1dce1a81f41157570f974a310f08
Alexander Pridava <sanyapridava@mail.ru>
2024-04-14 18:25:02 +0300

    useEffect request list program

--- gui/src/components/WindowFiles.tsx
+++ gui/src/components/WindowFiles.tsx
@@ -1,8 +1,16 @@
+import { useEffect,useState } from "react";
 import { API } from "../API/APImethod";
 
 export default function WindowFiles({children}: any) {
     
     const getListPrograms = ()=>API.getListPrograms(); //Method what request list files program C#
+
+    const [list_program, setListProgram]:[string[], any] =  useState([""]);
+
+    useEffect(()=>{
+        getListPrograms();
+    },[])
+
 
     return <div id={"background"} className={"bg-black flex w-full h-3"} style={{}}>
         <div id={"Choice"} className={"bg-white rounded relative"}>

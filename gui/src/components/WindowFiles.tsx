d0b2ea2ddf0c56bb57ef7e66d016ff989ac73cf1
Alexander Pridava <sanyapridava@mail.ru>
2024-04-16 12:46:12 +0300

    Change style close_button

--- gui/src/components/WindowFiles.tsx
+++ gui/src/components/WindowFiles.tsx
@@ -1,29 +1,37 @@
 import { useEffect,useState } from "react";
 import axios from "axios";
-
-interface DataUnit {
-    lang: string,
-    author: string
-}
+import iconfile from '../images/file.png';
 
 export default function WindowFiles({children}: any) {
     
     
 
-    const [list_program, setListProgram]:[DataUnit, any] =  useState({
-        lang: "", author: ""
-    });
+    const [list_program, setListProgram]:[string[], any] =  useState([]);
 
     useEffect(()=>{
-        axios.get("http://localhost:5123")
+        axios.get("http://localhost:5123/Translator/ListFiles")
             .then(res=>setListProgram(res.data))
+            .catch(err=>setListProgram(["Server not found"]))
+
     },[])
 
 
     return <div id={"background"} className={"bg-black flex w-full h-3"} style={{}}>
-        <div id={"Choice"} className={"bg-white rounded relative"}>
+        <div id={"Choice"} className={"bg-white rounded"}>
             {children}
-            <p>Язык: {list_program.lang} Автор: {list_program.author}</p>
+            {list_program.map((filename)=>{
+                
+                if (filename == "Server not found") 
+                    return (
+                        <h1>{filename}</h1>
+                
+                    )
+                
+                return <div className="flex gap-2 pl-3 hover:bg-gray-200">
+                    <img src={iconfile} className={'w-5 h-5'}></img>
+                    <p className={'h-10 filename'}>{filename}</p>
+                </div>})
+            }
         </div>
     </div>
     
--- gui/src/components/WindowFiles/ButtonClose.tsx
+++ gui/src/components/WindowFiles/ButtonClose.tsx
@@ -2,8 +2,8 @@ import {ButtonProps} from "../interfaces/ButtonProps";
 import crest from '../../images/crest.png';
 
 export default function ButtonClose({onClick}: ButtonProps) {
-    return <button onClick={onClick} className={"bg-red-500 hover:bg-red-700 text-white " +
-        "font-bold py-2 px-4 rounded-xl absolute right-2 top-2 "} style={{borderRadius: "40px"}}>
+    return <button id={'close_button'} onClick={onClick} className={"bg-red-500 hover:bg-red-700 text-white " +
+        "font-bold py-2 px-4 rounded-xl right-2 top-2"} style={{borderRadius: "40px", width: "max-content"}}>
         <img src={crest} className={"w-3 h-3"}/>
     </button>
 }

7e745a376cc7319bdaf54fc354fb8cc7bb4bc80f
Alexander Pridava <sanyapridava@mail.ru>
2024-05-22 16:22:26 +0300

    Change WindowFiles.tsx

--- gui/src/components/WindowFiles.tsx
+++ gui/src/components/WindowFiles.tsx
@@ -4,6 +4,8 @@ import iconfile from '../images/file.png';
 
 export default function WindowFiles({children}: any) {
     
+    
+
     const [list_program, setListProgram]:[string[], any] =  useState([]);
 
     useEffect(()=>{
@@ -22,15 +24,6 @@ export default function WindowFiles({children}: any) {
             .then(res=>console.log(res.data))
             .catch(err=>console.log("File not found!"));
 
-
-    const getTextProgram = (filename: string)=>
-        axios.get('http://localhost:5153/Translator/LexAnalys', {
-            params: {
-                filename: filename
-            }
-        })
-            .then(res=>console.log(res.data))
-            .catch(err=>console.log("File not found!"));
 
     return <div id={"background"} className={"bg-black flex w-full h-3"} style={{}}>
         <div id={"Choice"} className={"bg-white rounded"}>

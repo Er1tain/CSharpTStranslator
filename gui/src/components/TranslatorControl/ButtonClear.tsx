4a03ba47e89b1a52108c8fea199464adf4976878
unknown <sanyapridava@mail.ru>
2024-05-27 01:25:02 +0300

    Change Bodies button: add/update new method

--- gui/src/components/TranslatorControl/ButtonClear.tsx
+++ gui/src/components/TranslatorControl/ButtonClear.tsx
@@ -2,14 +2,9 @@
 
 export default function ButtonClear({onClick}: any) {
 
-    const ClearWindows = ()=>{
-        onClick({
-            "program_text": "",
-            "code_leksems": ""
-        })
-    }
+    
 
-    return <button onClick={ClearWindows} className={"bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"}>
+    return <button onClick={onClick} className={"bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"}>
         Очистить
     </button>
 }
--- gui/src/components/TranslatorControl/ButtonRun.tsx
+++ gui/src/components/TranslatorControl/ButtonRun.tsx
@@ -1,8 +1,12 @@
 import start from '../../images/triangle.png'
-import {ButtonProps} from "../interfaces/ButtonProps";
 
-export default function ButtonRun({onClick}: ButtonProps) {
-    return <button onClick={onClick} className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}>
+export default function ButtonRun({onClick}: any) {
+
+    const setVisible = ()=> {
+        onClick(false)
+    }
+
+    return <button onClick={setVisible} className={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}>
         <img src={start} className={"w-7 h-7 rotate-90"}/>
     </button>
 }

5907607fbc3f4ef9ae8d5bc05df09e7e4850aff5
unknown <sanyapridava@mail.ru>
2024-05-28 00:32:56 +0300

    Component on new route for translate in PolishNotation

--- gui/src/PolishNotation.tsx
+++ gui/src/PolishNotation.tsx
@@ -1,7 +1,19 @@
+import App from "./App";
+import Window from "./components/Window";
 
 export function PolishNotation() {
 
     return <div>
-        <h1>Hello</h1>
+        <h1 className={'text-center text-xl p-10'}>Полькая нотация</h1>
+        <div className={'grid grid-cols-2 ml-20'}>
+            <Window label={"Коды лексем"} className={`Window bg-blue-950 w-3/4 h-80 rounded font-bold text-yellow-400`}>
+                <></>
+            </Window>
+
+            <Window label={"Обратная польская запись"} className={`Window bg-blue-950 w-3/4 h-80 rounded font-bold text-yellow-400`}>
+                <></>
+            </Window>
+        </div>
+
     </div>
 }
